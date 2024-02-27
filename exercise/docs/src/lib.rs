ad69ba2a82a88b0925ccf12e566b572c7f4547aa
Thuyen Code <thuyencode@proton.me>
2024-02-27 16:25:34 +0700

    Complete exercise: Documentation

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -15,8 +15,19 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+//! A pumpkin, in English-language vernacular, is a cultivated winter squash in the genus Cucurbita. The term is most commonly applied to round, orange-colored squash varieties, though it does not possess a scientific definition and may be used in reference to many different squashes of varied appearance.
+//!
+//! ![An image of a pumpkin pile](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg)
+
+/// Big orange thing
+///
+/// # Recipes
+///
+/// Recipes will be coming soon.
 pub struct Pumpkin {
+    /// `roundness` is a percentage
     pub roundness: f32,
+    /// `orangeness` is a number from 8 to 27
     pub orangeness: i32,
 }
 
@@ -24,12 +35,14 @@ pub struct Pumpkin {
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    /// If you smash the pumpkin, it will be gone. Then it can't be used for pie 😢
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// `BURNT_ORANGE` is for the `orangeness` field in the [`Pumpkin`] struct.
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
@@ -38,6 +51,8 @@ pub const BURNT_ORANGE: i32 = 13;
 /// For internal use only. In fact, this documentation is so private that it won't be generated.
 /// At least not by default. But if you pass the correct option in, it will magically appear!
 #[allow(dead_code)] // to silence the warning
+
+// Use `cargo doc --release --document-private-items --no-deps`
 enum PrivateEnum {
     /// For Halloween. To be lit by candlelight.
     JackOLantern,
